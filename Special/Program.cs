6406245224b94c152097848b30525198dce03024
Catarina Fuscaliuc <catarinafuscaliuc.dev@gmail.com>
2024-03-16 19:44:43 +0000

    Special Sequence implemented into a code

--- Special/Program.cs
+++ Special/Program.cs
@@ -6,7 +6,21 @@ namespace Special
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Insert n-th number of the special sequence: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int result = GetSpecial(n);
+            Console.WriteLine(result);
+        }
+
+        private static int GetSpecial(int n)
+        {
+            if (n == 0)
+                return 0;
+            else if (n == 1)
+                return 1;
+            else
+                return 2 * GetSpecial(n - 1) + GetSpecial(n - 2);
         }
     }
 }
