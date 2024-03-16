ac8256e7fcc27b94977b1ae93ef231b9a8669584
Catarina Fuscaliuc <catarinafuscaliuc.dev@gmail.com>
2024-03-16 19:21:48 +0000

    XML Documentation Comments added

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -2,17 +2,16 @@
 
 namespace BetterDecorator
 {
-    static class StringExtensions
-    {
-        public static string Decorate(this string text, char character, int times)
-        {
-            string decoration = new string(character, times);
-            return $"{decoration} {text} {decoration}";
-        }
-    }
-
+    /// <summary>
+    /// Classe principal que contém o método de inicialização do programa.
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Método de entrada do programa.
+        /// Aceita três argumentos da linha de comando e usa-os para decorar a string fornecida.
+        /// </summary>
+        /// <param name="args">Argumentos da linha de comando: [0] - texto, [1] - caractere, [2] - número de vezes.</param>
         static void Main(string[] args)
         {
             if (args.Length < 3)
@@ -25,8 +24,21 @@ namespace BetterDecorator
             char character = args[1][0];
             int times = int.Parse(args[2]);
 
-            string decoratedText = text.Decorate(character, times);
+            string decoratedText = Decorate(text, character, times);
             Console.WriteLine(decoratedText);
+        }
+
+        /// <summary>
+        /// Método que decora uma string fornecida com um caractere especificado repetido um certo número de vezes de cada lado.
+        /// </summary>
+        /// <param name="text">Texto a ser decorado.</param>
+        /// <param name="character">Caractere usado para decoração.</param>
+        /// <param name="times">Número de vezes que o caractere deve ser repetido de cada lado da string.</param>
+        /// <returns>A string decorada.</returns>
+        static string Decorate(string text, char character, int times)
+        {
+            string decoration = new string(character, times);
+            return $"{decoration} {text} {decoration}";
         }
     }
 }
