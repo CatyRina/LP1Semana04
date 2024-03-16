259758b18036842c6a99b244f3e5b5fe05b05bb0
Catarina Fuscaliuc <catarinafuscaliuc.dev@gmail.com>
2024-03-16 19:16:41 +0000

    Attempt to add project to GitHub

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -2,11 +2,32 @@
 
 namespace BetterDecorator
 {
+    static class StringExtensions
+    {
+        public static string Decorate(this string text, char character, int times)
+        {
+            string decoration = new string(character, times);
+            return $"{decoration} {text} {decoration}";
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Uso: dotnet run --project BetterDecorator \"texto\" \"caractere\" número");
+                return;
+            }
+
+            string text = args[0];
+            char character = args[1][0];
+            int times = int.Parse(args[2]);
+
+            string decoratedText = text.Decorate(character, times);
+            Console.WriteLine(decoratedText);
         }
     }
 }
+
